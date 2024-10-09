4d1dedbc60b973be308d98ebb12edb07799965b2
Michal <michal.kalmanovitz@gmail.com>
2024-10-09 15:43:53 +0300

    cange images names

--- consts.py
+++ consts.py
@@ -3,7 +3,9 @@ WINDOW_WIDTH = 750
 FONT_SIZE = 20
 MSG_LOCATION = (10, WINDOW_HEIGHT - FONT_SIZE - 10)
 FONT_NAME = "Calibri"
-
+BINS_AMOUNT = 5
+# needs to add bins images
+BIN_LS = []
 # loading objects
 
 # adding objects to lists according categories
--- main.py
+++ main.py
@@ -3,29 +3,34 @@ import consts
 
 screen = pygame.display.set_mode(
     (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+
 def draw_message(message, font_size, color, location):
     font = pygame.font.SysFont(consts.FONT_NAME, font_size)
     text_img = font.render(message, True, color)
     screen.blit(text_img, location)
     pygame.display.flip()
+def handle_events():
+    global run
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
 
 level_chosen = False
+run = True
 def menu():
-    global level_chosen
+    global level_chosen, run
     pygame.init()
-    draw_message("Hello world", consts.FONT_SIZE, (255,255,255), consts.MSG_LOCATION)
-    pygame.time.delay(1000)
-    while not level_chosen:
-        event = pygame.event.wait()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_2:
-                level_chosen = True
-            elif event.key == pygame.K_3:
-                level_chosen = True
-            elif event.key == pygame.K_4:
-                level_chosen = True
-            elif event.key == pygame.K_5:
-                level_chosen = True
+    handle_events()
+    while run:
+        draw_message("Click Space to Continue", consts.FONT_SIZE, (255,255,255), consts.MSG_LOCATION)
+        while not level_chosen:
+            event = pygame.event.wait()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    level_chosen = True
+
+
 
 
 
